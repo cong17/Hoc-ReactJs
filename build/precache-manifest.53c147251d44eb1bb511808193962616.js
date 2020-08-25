446a3047108fc570fd6ab0f13658221ed3d86890
congkxtb98 <congkxtb98@gmail.com>
2020-08-25 22:26:41 +0700

    first commit

---
+++ build/precache-manifest.53c147251d44eb1bb511808193962616.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7489d1c0862b9e3f0c710b4afa0bd5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa81ff555e617b3134a1",
+    "url": "/static/css/main.d2da990e.chunk.css"
+  },
+  {
+    "revision": "89a3750145f5e6dab94c",
+    "url": "/static/js/2.d3d84a87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d3d84a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa81ff555e617b3134a1",
+    "url": "/static/js/main.b8404034.chunk.js"
+  },
+  {
+    "revision": "e345c23654b04d0263ee",
+    "url": "/static/js/runtime-main.f5e7bbac.js"
+  }
+]);
